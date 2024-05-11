29495dc95b666a59390eec88cb1a05e30558aeb6
justgotothedesk <wlscjf3877@naver.com>
2024-05-11 23:41:37 +0900

    feat: HTTP 라우트 추가

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/','App\Http\Controllers\MainController@index');
+
+// Route::get('/','MainController@index')->name('index');
+Route::get('/create','MainController@create')->name('create');
+Route::post('/store','MainController@store')->name('store');
+Route::get('/edit','MainController@edit')->name('edit');
+Route::post('/update','MainController@update')->name('update');
+Route::get('/delete','MainController@delete')->name('delete');
