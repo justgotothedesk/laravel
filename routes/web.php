d5c57dda7ff8b9308c0aefb8b006e1713e8a97be
justgotothedesk <wlscjf3877@naver.com>
2024-05-12 16:37:53 +0900

    fix: 라우팅 수정

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\MainController as MainController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/','App\Http\Controllers\MainController@index');
-
-// Route::get('/','MainController@index')->name('index');
+Route::get('/','MainController@index')->name('index');
 Route::get('/create','MainController@create')->name('create');
 Route::post('/store','MainController@store')->name('store');
 Route::get('/edit','MainController@edit')->name('edit');
