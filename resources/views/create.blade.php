0620bac431216a7a4acce4fd10a48c4e3107f7d2
justgotothedesk <wlscjf3877@naver.com>
2024-05-13 19:18:29 +0900

    fix: 들여쓰기 수정

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,14 +1,14 @@
 @extends('layout')
 @section('content')
     <form method="POST" action="{{ route('store') }}">
-    @crsf
+        @csrf
         <textarea name="content" rows="4"></textarea>
         @if($errors->any())
             @foreach($errors->all() as $error)
-            <p>{{ $error }}</p>
+                <p>{{ $error }}</p>
             @endforeach
         @endif
         <input type="submit" value="create">
-        <input type="button" value="back" onclick="location.href='{{ route('index')}}'">
+        <input type="button" value="back" onclick="location.href='{{ url('/') }}'">
     </form>
 @endsection
